b6aeb03c386ab76e32c84da3d60c3a23a660f125
rumbush <s.rumianyi@gmail.com>
2020-12-04 12:14:04 +0200

    add my hw files

---
+++ a.py
@@ -0,0 +1,6 @@
+number = float(input())
+
+if number % 2==0:
+    print ('parne=)')
+else:
+    print ('neparne')
---
+++ my_homework_26_11.py
@@ -0,0 +1,11 @@
+
+a = int(input('Please, enter some num for me =) ', ))
+b = int(input('...and one more, please ', ))
+
+print ('a+b=', a+b)
+print (a-b)
+print (a*b)
+print (a/b)
+print (a**b)
+print (a//b)
+print (a%b)
