5aa3cb2c3366bc4bbfedd62ada0050e6c3e43dd1
Sviatoslav <s.rumianyi@gmail.com>
2020-12-11 17:37:10 +0200

    hw4

--- HW4/task_2.py
+++ HW4/task_2.py
@@ -1,4 +1,4 @@
-# using list.append
+# using .append()
 numbers = [x*x for x in range (1, 7)]
 flt_numbers = []
 
@@ -8,7 +8,7 @@ for n in numbers:
 print (numbers)
 print (flt_numbers)
 
-# using list comprehension
+# list comprehension
 my_numbers = [x*x for x in range (2, 9)]
 flt_num = [float(n) for n in my_numbers]
 
